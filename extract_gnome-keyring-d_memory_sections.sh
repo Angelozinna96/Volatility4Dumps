84c57e7623d41d14fa218e94631a6ff459b2d56e
Unknown <angelozinna96@gmail.com>
2020-03-02 14:41:47 +0100

    added username searcher

--- extract_gnome-keyring-d_memory_sections.sh
+++ extract_gnome-keyring-d_memory_sections.sh
@@ -6,6 +6,11 @@ PATH_SAVE="./"
 echo "seaching gnome-keyring-d pid..."
 RES=($(python $VOL_LOC/vol.py -f $DUMP_LOC --profile=$PROF_NAME linux_pslist |grep gnome-keyring-d))
 pid=${RES[2]}
+if [ -z "$pid" ]
+then
+	echo "gnome-keyring-d pid not found!"
+	exit 0
+fi
 echo "gnome-keyring-d pid="$pid
 echo "seaching gnome-keyring-d potential sectors..."
 RES=($(python $VOL_LOC/vol.py -f $DUMP_LOC --profile=$PROF_NAME linux_proc_maps -p $pid |tail -8 |head -3))
@@ -17,10 +22,14 @@ for i in "${!RES[@]}";do
 done
 
 for addr in "${!ADDRESSES[@]}";do
-	echo "potential_start_addr_"$addr+1" = "${ADDRESSES[addr]}
+	echo "potential_start_addr_"$addr" = "${ADDRESSES[addr]}
 	echo "extration of the memory sector..."
 	RES=$(python $VOL_LOC/vol.py -f $DUMP_LOC --profile=$PROF_NAME linux_dump_map -p $pid -s ${ADDRESSES[addr]} --dump-dir=$PATH_SAVE)
 done
 echo "running python script for searching potential passwords..."
+ls $PATH_SAVE"task."$pid"."*
 python3 search_possible_pass_from_gdm-kering-d.py $(ls $PATH_SAVE"task."$pid"."*)
+echo "possible usernames:"
+strings $DUMP_LOC |grep -m 10 ^USERNAME= |sort |uniq
+
 rm -f $PATH_SAVE"task."$pid"."*
