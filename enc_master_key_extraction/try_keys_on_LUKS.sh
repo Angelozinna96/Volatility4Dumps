818a6ae8cde6499d77bfb263c3b83f529c089415
Unknown <angelozinna96@gmail.com>
2020-03-06 18:47:00 +0100

    only good key printed
    now the scripts only prints the good key

--- enc_master_key_extraction/try_keys_on_LUKS.sh
+++ enc_master_key_extraction/try_keys_on_LUKS.sh
@@ -3,7 +3,7 @@ PART="/dev/sda2"
 while read -r line; do
 
 	$( printf $line | xxd -r -ps > key_file)
-	sudo cryptsetup luksOpen --master-key-file key_file --test-passphrase "$PART" && echo "Found=$line"
+	sudo cryptsetup luksOpen --master-key-file key_file --test-passphrase "$PART" 2> /dev/null && echo "Found=$line"
 	rm -f key_file
 done < possible_keys
 printf "" | xxd -r -ps > key_file
