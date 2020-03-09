85eb2c6b53a16905e6b497168581334223f5faa4
Unknown <angelozinna96@gmail.com>
2020-03-09 10:12:31 +0100

    readme updated and try_keys_on_luks now takes path as a parameter

--- enc_master_key_extraction/try_keys_on_LUKS.sh
+++ enc_master_key_extraction/try_keys_on_LUKS.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-PART="/dev/sda2"
+PART="$1"
+KEYS_FILE="possible_keys"
+if [ "$1" == "" ] || [ $# -gt 1 ]; then
+        PART="/dev/sda2"
+fi
 while read -r line; do
 
 	$( printf $line | xxd -r -ps > key_file)
 	sudo cryptsetup luksOpen --master-key-file key_file --test-passphrase "$PART" 2> /dev/null && echo "Found=$line"
 	rm -f key_file
-done < possible_keys
+done < "$KEYS_FILE"
 printf "" | xxd -r -ps > key_file
