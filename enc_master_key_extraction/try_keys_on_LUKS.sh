4140f50b9a7c58dbffe400a690604460ddd1c294
Unknown <angelozinna96@gmail.com>
2020-03-10 11:23:22 +0100

    fix under Lorenzo Tip
    the variable PART was able to setup by command line argument

--- enc_master_key_extraction/try_keys_on_LUKS.sh
+++ enc_master_key_extraction/try_keys_on_LUKS.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-PART="$1"
+PART=$1
 KEYS_FILE="possible_keys"
-if [ "$1" == "" ] || [ $# -gt 1 ]; then
-        PART="/dev/sda2"
-fi
-while read -r line; do
 
-	$( printf $line | xxd -r -ps > key_file)
-	sudo cryptsetup luksOpen --master-key-file key_file --test-passphrase "$PART" 2> /dev/null && echo "Found=$line"
-	rm -f key_file
+if [$1 == ""] || [$# -gt 1]; then
+        PART="/dev/sdb2"
+fi
+
+while read -r line; do
+    $( printf $line | xxd -r -ps > key_file)
+    sudo cryptsetup luksOpen --master-key-file key_file --test-passphrase "$PART" 2> /dev/null && echo "Found=$line"
+    rm -f key_file
 done < "$KEYS_FILE"
+
 printf "" | xxd -r -ps > key_file
