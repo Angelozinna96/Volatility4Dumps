7f0e430b1c5d15459f41f1a17f48e59bd4b5048d
Unknown <angelozinna96@gmail.com>
2020-04-11 10:20:02 +0200

    added " everywhere

--- user_password_extraction/extract_gnome-keyring-d_memory_sections.sh
+++ user_password_extraction/extract_gnome-keyring-d_memory_sections.sh
@@ -4,7 +4,7 @@ DUMP_LOC=$2
 VOL_LOC=$3
 PATH_SAVE="./"
 echo "seaching gnome-keyring-d pid..."
-RES=($(python $VOL_LOC/vol.py -f $DUMP_LOC --profile=$PROF_NAME linux_pslist |grep gnome-keyring-d))
+RES=($(python "$VOL_LOC"/vol.py -f "$DUMP_LOC" --profile="$PROF_NAME" linux_pslist |grep gnome-keyring-d))
 pid=${RES[2]}
 if [ -z "$pid" ]
 then
@@ -13,7 +13,7 @@ then
 fi
 echo "gnome-keyring-d pid="$pid
 echo "seaching gnome-keyring-d potential sectors..."
-RES=($(python $VOL_LOC/vol.py -f $DUMP_LOC --profile=$PROF_NAME linux_proc_maps -p $pid |tail -8 |head -3))
+RES=($(python "$VOL_LOC"/vol.py -f "$DUMP_LOC" --profile="$PROF_NAME" linux_proc_maps -p $pid |tail -8 |head -3))
 ADDRESSES=()
 for i in "${!RES[@]}";do
 	if [ ${RES[i]} == "gnome-keyring-d" ];then
@@ -24,12 +24,12 @@ done
 for addr in "${!ADDRESSES[@]}";do
 	echo "potential_start_addr_"$addr" = "${ADDRESSES[addr]}
 	echo "extration of the memory sector..."
-	RES=$(python $VOL_LOC/vol.py -f $DUMP_LOC --profile=$PROF_NAME linux_dump_map -p $pid -s ${ADDRESSES[addr]} --dump-dir=$PATH_SAVE)
+	RES=$(python "$VOL_LOC"/vol.py -f "$DUMP_LOC" --profile="$PROF_NAME" linux_dump_map -p $pid -s ${ADDRESSES[addr]} --dump-dir="$PATH_SAVE")
 done
 echo "running python script for searching potential passwords..."
 ls $PATH_SAVE"task."$pid"."*
 python3 search_possible_pass_from_gdm-kering-d.py $(ls $PATH_SAVE"task."$pid"."*)
 echo "possible usernames:"
-strings $DUMP_LOC |grep -m 10 ^USERNAME= |sort |uniq
+strings "$DUMP_LOC" |grep -m 10 ^USERNAME= |sort |uniq
 
 rm -f $PATH_SAVE"task."$pid"."*
