4e32a6388d6ec808299445371377ead8b6f8a270
Unknown <angelozinna96@gmail.com>
2020-05-01 13:00:14 +0200

    save of the correct key

--- enc_master_key_extraction/try_keys_on_LUKS.sh
+++ enc_master_key_extraction/try_keys_on_LUKS.sh
@@ -8,7 +8,7 @@ fi
 
 while read -r line; do
     $( printf $line | xxd -r -ps > key_file)
-    sudo cryptsetup luksOpen --master-key-file key_file --test-passphrase "$PART" 2> /dev/null && echo "Found=$line"
+    sudo cryptsetup luksOpen --master-key-file key_file --test-passphrase "$PART" 2> /dev/null && echo "Found=$line" && cp key_file found_key
     rm -f key_file
 done < "$KEYS_FILE"
 
